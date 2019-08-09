2bd13d191c29f440bd6047e9ee0e3a89ae62ff1a
Julie Jonak <juliejonak10@gmail.com>
2019-08-09 09:05:30 -0700

    Updates ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,6 +1,6 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
-                        hash_table_insert
+                        hash_table_insert,
                         hash_table_retrieve,
                         hash_table_resize)
 
@@ -14,19 +14,25 @@ def get_indices_of_item_weights(weights, length, limit):
     # This solution does not account for resizing the hashtable if the length is >16.
     # Refactoring would include handling those edge cases
     
+    # sets up a hashtable with each weight as the key and the index as the value
     for i in range(0, length):
         hash_table_insert(ht, weights[i], i)
     
 
     for i in range(0, length):
+        # finds what the complimentary weight would be for item i
         complimentary_weight = limit - weights[i]
 
+        # if complimentary weight exists
         if hash_table_retrieve(ht, complimentary_weight):
+            # if i is the greater value, returns it as the first item in the tuple
             if i > hash_table_retrieve(ht, complimentary_weight):
                 return (i, hash_table_retrieve(ht, complimentary_weight))
+            # else if i is the lower value, returns it as the second item in the tuple
             else:
                 return (hash_table_retrieve(ht, complimentary_weight), i)
 
+    # if the key doesn't exist, returns None
     return None
 
 
